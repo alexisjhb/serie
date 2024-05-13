744b9a07d3107e03bee9db9248a37254e946b2bc
alexisjhb <169068450+alexisjhb@users.noreply.github.com>
2024-05-13 20:42:30 +0000

    Initial commit

---
+++ src/main/java/Main.java
@@ -0,0 +1,14 @@
+
+public class Main {
+  public static void main(String[] args) {
+System.out.println("*+*+*+*Serie: The Last of Us+*+*+*");
+
+System.out.println("");    
+  
+System.out.println("----the Last of Us é uma série que se passa em um mundo pós-apocalíptico devastado por uma epidemia que transformou a maioria da população em criaturas hostis e semelhantes a zumbis. A trama segue Joel, um sobrevivente endurecido pelo sofrimento, e Ellie, uma jovem corajosa que pode deter a chave para a cura da infecção. Juntos, eles embarcam em uma jornada perigosa e emocional através de paisagens desoladas, enfrentando não apenas as ameaças dos infectados, mas também os perigos representados por grupos de sobreviventes violentos e desesperados. No cerne da história está o vínculo entre Joel e Ellie, que se desenvolve à medida que enfrentam desafios, traumas e dilemas morais em sua busca por esperança em um mundo caído.----");
+  
+System.out.println("");
+  
+System.out.println("trailer oficial: https://youtu.be/lW5kiEUVlpo?si=YrX4XGsl_RvNaJjD");
+  }
+}
